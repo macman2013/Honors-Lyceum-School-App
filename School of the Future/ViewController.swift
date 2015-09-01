bbef5f3179b218fcf6f750dcc18adf4d66555113
Dale M Keith <dmkeith@Dales-MacBook-Pro.local>
2015-09-01 19:17:30 -0400

    Initial Commit

---
+++ School of the Future/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  School of the Future
+//
+//  Created by Dale M Keith on 9/1/15.
+//  Copyright (c) 2015 Honors 3034. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ School of the FutureTests/School_of_the_FutureTests.swift
@@ -0,0 +1,36 @@
+//
+//  School_of_the_FutureTests.swift
+//  School of the FutureTests
+//
+//  Created by Dale M Keith on 9/1/15.
+//  Copyright (c) 2015 Honors 3034. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class School_of_the_FutureTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
